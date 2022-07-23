0ee63b1cc74a7005e7af6871b711a10d23a1030e
ZlqPC <2232792471@qq.com>
2022-07-23 22:01:16 +0800

    串口通信未完成

--- Src/6usart/usart_synchronous.c
+++ Src/6usart/usart_synchronous.c
@@ -33,24 +33,8 @@ void init() {
     // 注：启用时钟先启用总的APB2外设时钟再启用下面的时钟一步一步
     // 1.使能usart时钟
     __HAL_RCC_USART1_CLK_ENABLE();
-    // 2.使能GPIO
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    // 3.初始化引脚PA9、PA10
-    GPIO_InitTypeDef typeDef = {0};
-    typeDef.Pin = GPIO_PIN_9;
-    // 由于是串口所以速度应该是最高的
-    typeDef.Speed = GPIO_SPEED_FREQ_HIGH;
-    // PA9是复用推挽输出（专门用于特殊的IO口）见原理图，由输出接输入
-    typeDef.Mode = GPIO_MODE_AF_PP;
-    // 初始化PA9
-    HAL_GPIO_Init(GPIOA, &typeDef);
-    typeDef.Pin = GPIO_PIN_10;
-    // PA10是复用输入模式
-    typeDef.Mode = GPIO_MODE_AF_INPUT;
-    // 初始化PA10
-    HAL_GPIO_Init(GPIOA, &typeDef);
 
-    // 4.设置USART的工作模式
+    // 2.设置USART的工作模式
     uartHandleTypeDef.Init.BaudRate = 9600;
     uartHandleTypeDef.Init.Parity = USART_PARITY_NONE;
     uartHandleTypeDef.Init.StopBits = USART_STOPBITS_1;
@@ -60,25 +44,48 @@ void init() {
     uartHandleTypeDef.Init.OverSampling = UART_OVERSAMPLING_16;
     HAL_UART_Init(&uartHandleTypeDef);
 
-    // 5.配置中断优先级组,系统默认是第四个，即四位最高可配置15
+    // 3.配置中断优先级组,系统默认是第四个，即四位最高可配置15
     HAL_NVIC_SetPriority(USART1_IRQn, 7, 0);
-    // 6.使能中断
+    // 4.使能中断
     HAL_NVIC_EnableIRQ(USART1_IRQn);
 
     deviceHandle();
 }
 
-void HAL_MspDeInit() {
-    __HAL_RCC_USART1_CLK_DISABLE();
-    __HAL_RCC_GPIOA_CLK_DISABLE();
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9);
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_10);
-    HAL_NVIC_DisableIRQ(USART1_IRQn);
+/**
+ * 覆写UART并初始化
+ */
+void HAL_UART_MspInit(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        // 1.使能GPIO
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        // 1.初始化引脚PA9、PA10
+        GPIO_InitTypeDef typeDef = {0};
+        typeDef.Pin = GPIO_PIN_9;
+        // 由于是串口所以速度应该是最高的
+        typeDef.Speed = GPIO_SPEED_FREQ_HIGH;
+        // PA9是复用推挽输出（专门用于特殊的IO口）见原理图，由输出接输入
+        typeDef.Mode = GPIO_MODE_AF_PP;
+        // 初始化PA9
+        HAL_GPIO_Init(GPIOA, &typeDef);
+        typeDef.Pin = GPIO_PIN_10;
+        // PA10是复用输入模式
+        typeDef.Mode = GPIO_MODE_AF_INPUT;
+        // 初始化PA10
+        HAL_GPIO_Init(GPIOA, &typeDef);
+    }
 }
 
-void start() {
-
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        __HAL_RCC_USART1_CLK_DISABLE();
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9);
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_10);
+        HAL_NVIC_DisableIRQ(USART1_IRQn);
+    }
 }
+
+void start() {}
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     HAL_UART_Transmit(huart, &data, 1, 0);
